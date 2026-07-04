d5f9f10f595cb8f7a825a4e59b2ce32b00d39469
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-04 12:07:59 +0000

    Initial commit

---
+++ src/test/java/com/financedomain/tracking/TrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financedomain.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
